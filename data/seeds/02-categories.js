6058661f5836faad738ae1b99434a560e5dd16db
justinelai <justine.lai@gmail.com>
2019-09-24 16:08:28 -0400

    seed health inputs

--- data/seeds/02-categories.js
+++ data/seeds/02-categories.js
@@ -3,8 +3,8 @@ exports.seed = function(knex) {
       return knex('categories').insert([
         {category_name: 'food'},
         {category_name: 'security'},
-        {category_name: 'debt'},
         {category_name: 'health'},
-        {category_name: 'transportation'}
+        {category_name: 'transportation'},
+        {category_name: 'debt'}
       ]);
 };
--- data/seeds/03-inputs.js
+++ data/seeds/03-inputs.js
@@ -4,9 +4,14 @@ exports.seed = function(knex) {
         {category_id: 1, input_name: "monthly_food"},
         {category_id: 1, input_name: "stock_up"},
         {category_id: 1, input_name: "dine_out"},
+
         {category_id: 2, input_name: "monthly_security"},
         {category_id: 2, input_name: "phone"},
         {category_id: 2, input_name: "change_locks"},
-        {category_id: 2, input_name: "extra_security"}
+        {category_id: 2, input_name: "extra_security"},
+
+        {category_id: 3, input_name: "monthly_health"},
+        {category_id: 3, input_name: "medication"},
+        {category_id: 3, input_name: "misc_health"}
       ]);
 };
